f36ce2384f6be90e57804d9d456e7c7c58e0c8af
SpaceMiningSystems <mark@spaceminingsystems.com>
2017-09-18 21:06:05 -0400

    Adding basic AI.

---
+++ DL_Prototype/Assets/EnemyAIScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAIScript : MonoBehaviour {
+
+
+
+    public GameObject goTarget;
+    public float speed = 1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, goTarget.transform.position, step);
+
+    }
+}
